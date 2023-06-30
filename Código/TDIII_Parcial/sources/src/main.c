4e4e375dc1cf9799a81f8d584396d698eb13f279
Juan Scarini <jd.scarini@gmail.com>
2023-06-30 18:35:50 -0300

    Se añaden comentarios.
    Tambien se reformula formula de calculo de RPM

--- Código/TDIII_Parcial/sources/src/main.c
+++ Código/TDIII_Parcial/sources/src/main.c
@@ -13,57 +13,65 @@
  ****************************************************************************/
 #define LCD_TASK_PRIORITY		            ( tskIDLE_PRIORITY + 1 )
 #define PULSOS_TASK_PRIORITY		            ( tskIDLE_PRIORITY + 2 )
-#define timeLedON 400
-#define timeLedOFF 600
 
-#define I2C_SDA_PIN 10
-#define I2C_SDA_PORT 0
-#define I2C_SCL_PIN 11
-#define I2C_SCL_PORT 0
+#define pulsosPorVuelta			12	//Cantidad de pulsos del encoder
+#define timeMaxQueuePulsos		20 	//Lo mínimo que puedo medir son xx RPM
+#define muestrasPromedio		24 	//promedio las ultimas 2 vueltas
+#define ticksEsperaLCD 			200	//Tiempo de refresco del display.
 
-#define GIRO_IZQ_PORT	2
-#define GIRO_IZQ_PIN	1
-#define GIRO_IZQ		2,1
-#define GIRO_DER_PORT	2
-#define GIRO_DER_PIN	2
-#define GIRO_DER		2,2
+/************************
+ * Definiciones para máquina de estados
+ */
+#define GIRO_FSM_S1				1
+#define GIRO_FSM_S2				2
+#define GIRO_FSM_S3				3
+#define GIRO_FSM_S4				4
 
-#define GIRO_FSM_S1		1
-#define GIRO_FSM_S2		2
-#define GIRO_FSM_S3		3
-#define GIRO_FSM_S4		4
+/************************
+ * Definiciones de pines
+ */
 
-#define BOTON1		0,27
-#define BOTON2		0,28
-#define BOTON1_PORT	0
-#define BOTON1_PIN	27
-#define BOTON2_PORT	0
-#define BOTON2_PIN	28
+#define I2C_SDA					0,10
+#define I2C_SDA_PIN 			10
+#define I2C_SDA_PORT 			0
+#define I2C_SCL					0,11
+#define I2C_SCL_PIN 			11
+#define I2C_SCL_PORT 			0
 
-#define pulsosPorVuelta	10
-#define timeMaxQueuePulsos	20 //Lo mínimo que puedo medir son xx RPM
-#define muestrasPromedio	24 //promedio las ultimas 2 vueltas
+#define GIRO_IZQ_PORT			2
+#define GIRO_IZQ_PIN			1
+#define GIRO_IZQ				2,1
+#define GIRO_DER_PORT			2
+#define GIRO_DER_PIN			2
+#define GIRO_DER				2,2
 
-#define ticksEsperaLCD 200
+#define BOTON1					0,27
+#define BOTON2					0,28
+#define BOTON1_PORT				0
+#define BOTON1_PIN				27
+#define BOTON2_PORT				0
+#define BOTON2_PIN				28
 
-#define divisorRPM	10
-//Agregar define i2c i2c3
+#define ENTRADAPULSOS			0,24
+#define ENTRADAPULSOS_PORT		0
+#define ENTRADAPULSOS_PIN		24
 
+/**************
+ * Definiciones de estructura envío de cola LCD
+ */
 typedef struct LCD_t {
 	char *mensaje;
 	unsigned int linea;
 	unsigned int posicion;
 } LCD_Struct;
 
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
 xQueueHandle xQueueLCD;
 xQueueHandle xQueueCapture;
 xQueueHandle xQueueGiro;
 xQueueHandle xQueueVelocidad;
-
-/*****************************************************************************
- * Public types/enumerations/variables
- ****************************************************************************/
-
 /*****************************************************************************
  * Private functions
  ****************************************************************************/
@@ -84,16 +92,11 @@ static void prvSetupHardware(void) {
 unsigned int actualizarVelocidad(unsigned int actualValue,
 		unsigned int lastValue, unsigned int clockPwm) {
 	unsigned int aux = 0;
-	//char mensaje[16] = {' '};
-	//bool	cambioVel = 0;
-
 	if (actualValue > lastValue + 2 || actualValue < lastValue - 2) {
 		/*comparo si vario el PWM + de 1%*/
 		aux = actualValue * 100 / 4096;
 		LPC_PWM1->MR1 = actualValue * clockPwm / 4096;
 		LPC_PWM1->LER = (1 << 0) | (1 << 1);
-		//cambioVel = 1;
-		//xQueueSend(xQueueLCD, &dataLCD, 10);
 	}
 	return aux;
 }
@@ -119,11 +122,12 @@ static void vGiro_Task(void *pvParameters) {
 
 	clockPwm = Chip_Clock_GetSystemClockRate() / 800; //Divido para obtener un pwm a 200hz, frec ideal para motor.
 
+	// Inicializo ADC en modo burst
 	Chip_ADC_Init(LPC_ADC, &ADCSetup);
 	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_PORT, ADC_PIN, 3);
 	Chip_ADC_EnableChannel(LPC_ADC, ADC_CHANNEL, ENABLE);
 	Chip_ADC_SetBurstCmd(LPC_ADC, ENABLE);
-
+	//Configuro pines de salida para dirección de giro
 	Chip_GPIO_SetDir(LPC_GPIO, GIRO_IZQ, 1);
 	Chip_GPIO_SetDir(LPC_GPIO, GIRO_DER, 1);
 	Chip_GPIO_SetPinState(LPC_GPIO, GIRO_DER, 0);
@@ -159,7 +163,7 @@ static void vGiro_Task(void *pvParameters) {
 				solicitudGiroIzq = 1;
 			}
 		}
-
+		//Máquina de estados para control de giro
 		switch (estadoActual) {
 		default:
 		case GIRO_FSM_S1:
@@ -295,7 +299,7 @@ static void vGiro_Task(void *pvParameters) {
 			}
 			break;
 		}
-
+		//Espero 20ms para dejar que las otras tarejas se puedan ejecutar.
 		vTaskDelayUntil(&xLastWakeTime, 20);
 		xLastWakeTime = xTaskGetTickCount();
 	}
@@ -306,40 +310,41 @@ static void vGiro_Task(void *pvParameters) {
 static void vPulsos_Task(void *pvParameters) {
 	unsigned int captura;
 	unsigned int rpm[muestrasPromedio] = { 0 }; //Guardo un array por si necesito hacer promedio.
-	unsigned int frecVuelta;
+	unsigned int frecVuelta = 10;
 	unsigned int i = 0, ii = 0, aux = 0;
 	LCD_Struct datosLcd;
 	uint32_t timerFreq;
 	char mensaje[16] = { ' ' };
 	TickType_t xLastWakeTime = 0;
 
+	//Configuro timer para tomar muestras por interrupcion. En modo Input Capture
 	Chip_Clock_SetPCLKDiv(SYSCTL_PCLK_TIMER3, SYSCTL_CLKDIV_1);
 	Chip_TIMER_Init(LPC_TIMER3);
 	timerFreq = Chip_Clock_GetSystemClockRate();
-
 	Chip_TIMER_CaptureEnableInt(LPC_TIMER3, 1);
 	Chip_TIMER_CaptureFallingEdgeEnable(LPC_TIMER3, 1);
-
 	Chip_TIMER_PrescaleSet(LPC_TIMER3, 0);
-
 	Chip_TIMER_Reset(LPC_TIMER3);
 	Chip_TIMER_Enable(LPC_TIMER3);
-
 	NVIC_ClearPendingIRQ(TIMER3_IRQn);
 	NVIC_EnableIRQ(TIMER3_IRQn);
-
+	//Configuro pin del timer como funcion timer.
 	Chip_IOCON_PinMux(LPC_IOCON, 0, 24, IOCON_MODE_PULLUP, IOCON_FUNC3);
 
 	while (1) {
+
 		if (xQueueReceive(xQueueCapture, &captura, timeMaxQueuePulsos) == pdPASS) //Si recibe pulsos antes del tiempo estamos a más de 60 RPM.
 		{
+
 			frecVuelta = timerFreq / captura;
-			rpm[i] = frecVuelta * 60 / (12);
+			rpm[i] = (frecVuelta * 10 / pulsosPorVuelta)*6;
 		} else {
 			rpm[i] = 0;
 			//captura = 0;
 			//debería resetear el timer si no llega el pulso? Probemos.
+
 		}
+
 		xQueueSend(xQueueVelocidad, &rpm[i], 0);
 
 		if ((xLastWakeTime + ticksEsperaLCD) <= xTaskGetTickCount()) {
@@ -361,6 +366,7 @@ static void vPulsos_Task(void *pvParameters) {
 		if (i >= muestrasPromedio) {
 			i = 0;
 		}
+		vTaskDelay(20);
 
 	}
 
@@ -381,7 +387,7 @@ static void vLCD_Task(void *pvParameters) {
 	Chip_IOCON_PinMuxSet(LPC_IOCON, I2C_SDA_PORT, I2C_SDA_PIN, 2);
 	Chip_IOCON_PinMuxSet(LPC_IOCON, I2C_SCL_PORT, I2C_SCL_PIN, 2);
 
-	vTaskDelay(300); // Le doy tiempo a la lógica del LCD para iniciar
+	//vTaskDelay(300); // Le doy tiempo a la lógica del LCD para iniciar
 
 	lcd_init(id);
 	lcd_clear();
@@ -429,15 +435,11 @@ int main(void) {
 
 	prvSetupHardware();
 
-	xTaskCreate(vGiro_Task, (char*) "vMensaje_Task",
-			configMINIMAL_STACK_SIZE , NULL, PULSOS_TASK_PRIORITY, NULL);
-	xTaskCreate(vPulsos_Task, (char*) "vPulsos_Task", configMINIMAL_STACK_SIZE ,
-			NULL, PULSOS_TASK_PRIORITY, NULL);
-	xTaskCreate(vPulsadores_Task, (char*) "vPulsadores_Task",
-			configMINIMAL_STACK_SIZE , NULL, PULSOS_TASK_PRIORITY, NULL);
+	xTaskCreate(vGiro_Task, (char*) "vGiro_Task",configMINIMAL_STACK_SIZE , NULL, PULSOS_TASK_PRIORITY, NULL);
+	xTaskCreate(vPulsos_Task, (char*) "vPulsos_Task", configMINIMAL_STACK_SIZE ,NULL, PULSOS_TASK_PRIORITY, NULL);
+	xTaskCreate(vPulsadores_Task, (char*) "vPulsadores_Task",configMINIMAL_STACK_SIZE , NULL, PULSOS_TASK_PRIORITY, NULL);
 
-	xTaskCreate(vLCD_Task, (char*) "vLCD_Task", configMINIMAL_STACK_SIZE , NULL,
-			LCD_TASK_PRIORITY, NULL);
+	xTaskCreate(vLCD_Task, (char*) "vLCD_Task", configMINIMAL_STACK_SIZE , NULL,LCD_TASK_PRIORITY, NULL);
 
 
 	xQueueLCD = xQueueCreate(5, sizeof(LCD_Struct));
